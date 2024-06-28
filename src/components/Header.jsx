020ef8de1fe5042ab4a578b11c5a075ffdcfbdcb
Harsh-Singh09 <harshrsingh552005@gmail.com>
2024-06-28 20:21:14 +0530

    Setup for Portfolio Webapp using Next.js

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import ThemeToggle from "@/components/ThemeToggle";
+
+const Header = () => {
+  return (
+    <>
+      <header className="">
+        <ThemeToggle />
+      </header>
+    </>
+  );
+};
+
+export default Header;
